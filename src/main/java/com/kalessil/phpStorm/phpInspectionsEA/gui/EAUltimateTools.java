42a402ed6077f46d8f7267739ff1dca8e04048de
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-22 14:37:20 +0200

    Ultimate: WIP, wiring settings management

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/gui/EAUltimateTools.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/gui/EAUltimateTools.java
@@ -4,6 +4,9 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateApplicationConfiguration;
+import com.kalessil.phpStorm.phpInspectionsEA.EAUltimateSettings;
+import com.kalessil.phpStorm.phpInspectionsEA.options.OptionsComponent;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.components.AbstractProjectComponent;
 
@@ -30,10 +33,21 @@ public class EAUltimateTools extends AbstractProjectComponent {
     @Override
     public void projectOpened() {
         if (!this.isInstantiated()) {
-            final ToolWindow window = this.windowManager.registerToolWindow(TOOL_WINDOW_ID, new JPanel(), ToolWindowAnchor.RIGHT);
+            final ToolWindow window = this.windowManager.registerToolWindow(TOOL_WINDOW_ID, this.buildPanel(), ToolWindowAnchor.RIGHT);
             window.setIcon(new ImageIcon(this.getClass().getResource("/logo_18x18.png")));
 
         }
+    }
+
+    @NotNull
+    private JPanel buildPanel() {
+        final EAUltimateSettings settings = EAUltimateSettings.getInstance();
+
+        return OptionsComponent.create(component -> {
+            component.addPanel("Distraction level", panel ->
+                    panel.addCheckbox("Analyze only modified files", settings.getCheckOnlyChangedFiles(), settings::setCheckOnlyChangedFiles)
+            );
+        });
     }
 
     @Override

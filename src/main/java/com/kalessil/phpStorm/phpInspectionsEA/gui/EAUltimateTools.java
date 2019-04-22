3314124cb7207ee619be7ad1056c3c73f5aac040
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-22 15:13:11 +0200

    Ultimate: WIP, added side-bar content

--- src/main/java/com/kalessil/phpStorm/phpInspectionsEA/gui/EAUltimateTools.java
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/gui/EAUltimateTools.java
@@ -1,5 +1,8 @@
 package com.kalessil.phpStorm.phpInspectionsEA.gui;
 
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
@@ -20,7 +23,7 @@ import javax.swing.*;
  */
 
 public class EAUltimateTools extends AbstractProjectComponent {
-    final private String TOOL_WINDOW_ID = "Inspections";
+    final private String TOOL_WINDOW_ID = "PHP Inspections";
     final private ToolWindowManager windowManager;
 
     protected EAUltimateTools(@NotNull Project project) {
@@ -33,6 +36,10 @@ public class EAUltimateTools extends AbstractProjectComponent {
         if (!this.isInstantiated()) {
             final ToolWindow window = this.windowManager.registerToolWindow(TOOL_WINDOW_ID, this.buildPanel(), ToolWindowAnchor.RIGHT);
             window.setIcon(new ImageIcon(this.getClass().getResource("/logo_18x18.png")));
+            final IdeaPluginDescriptor plugin = PluginManager.getPlugin(PluginId.getId("com.kalessil.phpStorm.phpInspectionsUltimate"));
+            if (plugin != null) {
+                window.setTitle(plugin.getName());
+            }
         }
     }
 
@@ -41,13 +48,14 @@ public class EAUltimateTools extends AbstractProjectComponent {
         final EAUltimateSettings settings = EAUltimateSettings.getInstance();
 
         return OptionsComponent.create(component -> {
-            component.addPanel("Distraction level (global)", panel ->
+            component.addPanel("License status",              panel -> {});
+            component.addPanel("Distraction level settings",  panel ->
                     panel.addCheckbox("Analyze only modified files", settings.getCheckOnlyChangedFiles(), settings::setCheckOnlyChangedFiles)
             );
-            component.addPanel("Active inspections groups (project)", panel -> {
+            component.addPanel("Active inspections groups",   panel -> {
                 panel.addCheckbox("Architecture",             true, (isSelected) -> {});
-                panel.addCheckbox("Control Flow",             true, (isSelected) -> {});
-                panel.addCheckbox("Code Style",               true, (isSelected) -> {});
+                panel.addCheckbox("Control flow",             true, (isSelected) -> {});
+                panel.addCheckbox("Code style",               true, (isSelected) -> {});
                 panel.addCheckbox("Language level migration", true, (isSelected) -> {});
                 panel.addCheckbox("Performance",              true, (isSelected) -> {});
                 panel.addCheckbox("Probable bugs",            true, (isSelected) -> {});

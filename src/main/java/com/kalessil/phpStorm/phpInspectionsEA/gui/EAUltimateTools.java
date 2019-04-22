f71c7de8b40e973f0896e99334bdf280fe63b934
Vladimir Reznichenko <kalessil@gmail.com>
2019-04-22 12:54:55 +0200

    Ultimate: WIP, adding a side-panel

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/gui/EAUltimateTools.java
@@ -0,0 +1,44 @@
+package com.kalessil.phpStorm.phpInspectionsEA.gui;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.wm.ToolWindowManager;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.components.AbstractProjectComponent;
+
+import javax.swing.*;
+
+/*
+ * This file is part of the Php Inspections (EA Extended) package.
+ *
+ * (c) Vladimir Reznichenko <kalessil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+public class EAUltimateTools extends AbstractProjectComponent {
+    final private String TOOL_WINDOW_ID = "Php Inspections (EA *)";
+    final private ToolWindowManager windowManager;
+
+    protected EAUltimateTools(@NotNull Project project) {
+        super(project);
+        this.windowManager = ToolWindowManager.getInstance(project);
+    }
+
+    @Override public void projectOpened() {
+        if (!this.isInstantiated()) {
+            this.windowManager.registerToolWindow(TOOL_WINDOW_ID, new JPanel(), ToolWindowAnchor.RIGHT);
+        }
+    }
+
+    @Override public void projectClosed() {
+        if (this.isInstantiated()) {
+            this.windowManager.unregisterToolWindow(TOOL_WINDOW_ID);
+        }
+    }
+
+    private boolean isInstantiated() {
+        return this.windowManager.getToolWindow(TOOL_WINDOW_ID) != null;
+    }
+}
